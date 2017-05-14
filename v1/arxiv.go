2e194aa6401bdbe006c401e4ea41cd4e6c52b41b
Emmanuel Odeke <emm.odeke@gmail.com>
2017-05-14 04:24:13 -0600

    all: implemented the API client
    Implemented simple & advanced searching with
    all the categories and filters supported by arxiv.org.
    
    Sample usage
    * Preamble:
    ```go
    package main
    
    import (
        "fmt"
        "log"
    
        "github.com/orijtech/arxiv/v1"
    )
    ```
    
    * Simple search
    ```go
    func simpleSearch() {
      resChan, cancel, err := arxiv.Search(&arxiv.Query{
        Terms:         "deep learning",
        MaxPageNumber: 5,
      })
      if err != nil {
        log.Fatal(err)
      }
    
      for resPage := range resChan {
        if err := resPage.Err; err != nil {
          fmt.Printf("#%d err: %v", resPage.PageNumber, err)
          continue
        }
    
        fmt.Printf("#%d\n", resPage.PageNumber)
        feed := resPage.Feed
        fmt.Printf("\tTitle: %s\n\tID: %s\n\tAuthor: %#v\n\tUpdated: %#v\n",
    feed.Title, feed.ID, feed.Author, feed.Updated)
        for i, entry := range feed.Entry {
          fmt.Printf("\n\t\tEntry: #%d Title: %s ID: %s\n\t\tSummary:
    %s\n\t\tContent: %#v\n\t\tUpdated: %#v\n\t\tLinks: %#v\n",
    	i, entry.Title, entry.ID, entry.Summary.Body, entry.Content,
    entry.Updated, entry.Link,
          )
        }
        if resPage.PageNumber >= 2 {
          cancel()
        }
      }
    }
    ```
    
    * Advanced/complex search
    ```go
    func advancedSearch() {
      resChan, cancel, err := arxiv.Search(&arxiv.Query{
        Filters: []*arxiv.Filter{
          &arxiv.Filter{
    	Op: arxiv.OpOR,
    	Fields: []*arxiv.Field{
    	  {Title: "architecture"},
    	  {Category: arxiv.CSGameTheory},
    	  {Comment: "biological"},
    	},
          },
        },
        MaxPageNumber: 2,
      })
      if err != nil {
        log.Fatal(err)
      }
    
      for resPage := range resChan {
        if err := resPage.Err; err != nil {
          fmt.Printf("#%d err: %v", resPage.PageNumber, err)
          continue
        }
    
        fmt.Printf("#%d\n", resPage.PageNumber)
        feed := resPage.Feed
        fmt.Printf("\tTitle: %s\n\tID: %s\n\tAuthor: %#v\n\tUpdated: %#v\n",
    feed.Title, feed.ID, feed.Author, feed.Updated)
        for i, entry := range feed.Entry {
          fmt.Printf("\n\t\tEntry: #%d Title: %s ID: %s\n\t\tSummary:
    %s\n\t\tContent: %#v\n\t\tUpdated: %#v\n\t\tLinks: %#v\n",
    	i, entry.Title, entry.ID, entry.Summary.Body, entry.Content,
    entry.Updated, entry.Link,
          )
        }
        if resPage.PageNumber >= 2 {
          cancel()
        }
      }
    }
    ```

---
+++ v1/arxiv.go
@@ -0,0 +1,294 @@
+// Copyright 2017 orijtech. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package arxiv
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"time"
+
+	"golang.org/x/tools/blog/atom"
+
+	"github.com/orijtech/otils"
+)
+
+type Client struct {
+	sync.RWMutex
+
+	rt http.RoundTripper
+}
+
+func (c *Client) httpClient() *http.Client {
+	c.RLock()
+	defer c.RUnlock()
+
+	rt := c.rt
+	if rt == nil {
+		rt = http.DefaultTransport
+	}
+	return &http.Client{Transport: rt}
+}
+
+func (c *Client) SetHTTPRoundTripper(rt http.RoundTripper) {
+	c.Lock()
+	defer c.Unlock()
+
+	c.rt = rt
+}
+
+const (
+	baseURL = "http://export.arxiv.org/api"
+)
+
+const recommendedThrottleDuration = time.Duration(3 * time.Second)
+
+type SortOrder string
+
+const (
+	SortAscending  SortOrder = "ascending"
+	SortDescending SortOrder = "descending"
+)
+
+type SortBy string
+
+const (
+	SortByRelevance       SortBy = "relevance"
+	SortBySubmittedDate   SortBy = "submittedDate"
+	SortByLastUpdatedDate SortBy = "lastUpdatedDate"
+)
+
+type Query struct {
+	MaxPageNumber int64 `json:"max_page"`
+
+	Terms string `json:"search_query"`
+
+	Filters Filters `json:"filters"`
+
+	PageNumber int64 `json:"start"`
+
+	MaxResultsPerPage int64 `json:"max_results"`
+
+	// ArticleIDs is the specific set of articles
+	// to be searched for by the API.
+	ArticleIDs []string `json:"id_list"`
+
+	SortBy    SortBy    `json:"sortBy"`
+	SortOrder SortOrder `json:"sortOrder"`
+
+	// ThrottleSeconds determines the period that it'll
+	// wait before making the next pagination request.
+	// arxiv.org recommends that you set it to 3 seconds
+	// to play nice. If this value is unset, the recommended
+	// 3 second throttle duration will be used.
+	// Set it to a negative value e.g -1 for no throttling.
+	ThrottleSeconds int64 `json:"throttle_seconds"`
+}
+
+// Fresh struct here to avoid sending
+// unnecessary query string values from Query
+type reqPage struct {
+	Terms      string   `json:"search_query"`
+	Start      int64    `json:"start"`
+	MaxResults int64    `json:"max_results"`
+	ArticleIDs []string `json:"id_list"`
+
+	SortBy    SortBy    `json:"sortBy"`
+	SortOrder SortOrder `json:"sortOrder"`
+}
+
+func (q *Query) reqPage() *reqPage {
+	return &reqPage{
+		Terms:      q.Terms,
+		Start:      q.PageNumber,
+		SortBy:     q.SortBy,
+		MaxResults: q.MaxResultsPerPage,
+		ArticleIDs: q.ArticleIDs[:],
+		SortOrder:  q.SortOrder,
+	}
+}
+
+var (
+	errNilQuery            = errors.New("non-nil query expected")
+	errExpectingIDsOrQuery = errors.New("expecting either ArticleIDs or Terms or Filters to have been set")
+)
+
+func (q *Query) Validate() error {
+	if q == nil {
+		return errNilQuery
+	}
+	if len(q.ArticleIDs) == 0 && q.Terms == "" && len(q.Filters) == 0 {
+		return errExpectingIDsOrQuery
+	}
+	return nil
+}
+
+type ResultsPage struct {
+	Feed *atom.Feed `json:"feed"`
+
+	PageNumber int64 `json:"page_number"`
+
+	Err error `json:"error"`
+}
+
+const (
+	defaultMaxResultsPerPage = 10
+)
+
+func (q *Query) prepareForPagination() {
+	// Pages are zero-based so anything
+	// less than that should be set to zero
+	if q.PageNumber <= 0 {
+		q.PageNumber = 0
+	}
+
+	if q.MaxResultsPerPage <= 0 {
+		q.MaxResultsPerPage = defaultMaxResultsPerPage
+	}
+}
+
+type cancelFn func()
+
+var defaultClient = new(Client)
+
+func Search(q *Query) (chan *ResultsPage, cancelFn, error) {
+	return defaultClient.Search(q)
+}
+
+func (c *Client) Search(q *Query) (chan *ResultsPage, cancelFn, error) {
+	if err := q.Validate(); err != nil {
+		return nil, nil, err
+	}
+
+	maxPageNumber := q.MaxPageNumber
+	maxPageExceeded := func(pageNumber int64) bool {
+		return maxPageNumber > 0 && pageNumber >= maxPageNumber
+	}
+
+	responsesChan := make(chan *ResultsPage)
+	cancelFn, cancelChan := makeCanceler()
+
+	var throttleDuration = recommendedThrottleDuration
+	secs := q.ThrottleSeconds
+	if secs >= 1 {
+		throttleDuration = time.Duration(secs) * time.Second
+	} else if secs <= -1 {
+		throttleDuration = 0 * time.Second
+	}
+
+	// Take out the filters on their own
+	filters := q.Filters[:]
+	filtersStr := filters.String()
+
+	go func() {
+		defer close(responsesChan)
+
+		pager := q
+		pager.prepareForPagination()
+
+		for {
+			respPage := new(ResultsPage)
+			respPage.PageNumber = pager.PageNumber
+			qv, err := otils.ToURLValues(pager.reqPage())
+			if err != nil {
+				respPage.Err = err
+				responsesChan <- respPage
+				return
+			}
+			if len(filters) > 0 {
+				qv.Set("search_query", filtersStr)
+			}
+
+			fullURL := fmt.Sprintf("%s/query?%s", baseURL, qv.Encode())
+			req, err := http.NewRequest("GET", fullURL, nil)
+			if err != nil {
+				respPage.Err = err
+				responsesChan <- respPage
+				return
+			}
+
+			slurp, _, err := c.doReqAndSlurp(req)
+			if err != nil {
+				respPage.Err = err
+				responsesChan <- respPage
+				return
+			}
+
+			feed := new(atom.Feed)
+			if err := xml.Unmarshal(slurp, feed); err != nil {
+				respPage.Err = err
+				responsesChan <- respPage
+				return
+			}
+
+			if feed == nil || len(feed.Entry) == 0 {
+				// No more content since pages must be contiguous
+				// before we encounter the first empty page.
+				break
+			}
+
+			respPage.Feed = feed
+			responsesChan <- respPage
+
+			pager.PageNumber += 1
+			if maxPageExceeded(pager.PageNumber) {
+				break
+			}
+
+			select {
+			case <-time.After(throttleDuration):
+			case <-cancelChan:
+				return
+			}
+		}
+	}()
+
+	return responsesChan, cancelFn, nil
+}
+
+func makeCanceler() (cancelFn, <-chan bool) {
+	cancelChan := make(chan bool)
+	var cancelOnce sync.Once
+	cancelFn := func() { cancelOnce.Do(func() { close(cancelChan) }) }
+	return cancelFn, cancelChan
+}
+
+func (c *Client) doReqAndSlurp(req *http.Request) ([]byte, http.Header, error) {
+	res, err := c.httpClient().Do(req)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	if !otils.StatusOK(res.StatusCode) {
+		msg := res.Status
+		if res.Body != nil {
+			slurp, _ := ioutil.ReadAll(res.Body)
+			if len(slurp) > 4 {
+				msg = string(slurp)
+			}
+		}
+		return nil, res.Header, errors.New(msg)
+	}
+
+	slurp, err := ioutil.ReadAll(res.Body)
+	return slurp, res.Header, err
+}
---
+++ v1/arxiv_test.go
@@ -0,0 +1,175 @@
+// Copyright 2017 orijtech. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package arxiv_test
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"testing"
+
+	"golang.org/x/tools/blog/atom"
+
+	"github.com/orijtech/arxiv/v1"
+)
+
+func TestSearch(t *testing.T) {
+	client := new(arxiv.Client)
+	backend := &testBackend{route: searchRoute}
+	client.SetHTTPRoundTripper(backend)
+
+	tests := [...]struct {
+		req       *arxiv.Query
+		wantErr   bool
+		wantPages []*arxiv.ResultsPage
+	}{
+		0: {
+			req: &arxiv.Query{
+				Terms:         "deep dream",
+				MaxPageNumber: 3,
+
+				ThrottleSeconds: -1,
+			},
+			wantPages: []*arxiv.ResultsPage{
+				resultsPageFromFile("deep dream", 0),
+				resultsPageFromFile("deep dream", 1),
+				resultsPageFromFile("deep dream", 2),
+			},
+		},
+		1: {
+			req:     nil,
+			wantErr: true,
+		},
+		2: {
+			req:     &arxiv.Query{},
+			wantErr: true,
+		},
+	}
+
+	for i, tt := range tests {
+		resPageChan, cancel, err := client.Search(tt.req)
+		if tt.wantErr {
+			if err == nil {
+				t.Errorf("#%d want non-nil error", i)
+			}
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("#%d:: got err: %v", i, err)
+			continue
+		}
+
+		if cancel == nil {
+			t.Errorf("#%d:: expecting a non-nil canceler", i)
+			continue
+		}
+
+		var gotPages []*arxiv.ResultsPage
+		for resPage := range resPageChan {
+			gotPages = append(gotPages, resPage)
+		}
+		cancel()
+
+		gotBlob, wantBlob := blobify(gotPages), blobify(tt.wantPages)
+		if !bytes.Equal(gotBlob, wantBlob) {
+			t.Errorf("#%d:\ngotBlob:  %s\nwantBlob: %s\n", i, gotBlob, wantBlob)
+		}
+	}
+}
+
+func blobify(v interface{}) []byte {
+	blob, _ := xml.Marshal(v)
+	return blob
+}
+
+const (
+	searchRoute = "search"
+)
+
+type testBackend struct {
+	route string
+}
+
+var _ http.RoundTripper = (*testBackend)(nil)
+
+func (tb *testBackend) RoundTrip(req *http.Request) (*http.Response, error) {
+	switch tb.route {
+	case searchRoute:
+		return tb.searchRoundTrip(req)
+	default:
+		return makeResp("unknown route", http.StatusNotFound), nil
+	}
+}
+
+func makeResp(status string, code int) *http.Response {
+	return &http.Response{
+		StatusCode: code,
+		Status:     status,
+		Body:       nil,
+	}
+}
+
+func (tb *testBackend) searchRoundTrip(req *http.Request) (*http.Response, error) {
+	query := req.URL.Query()
+	terms := query.Get("search_query")
+	if terms == "" {
+		return makeResp(`expecting "search_query"`, http.StatusBadRequest), nil
+	}
+
+	pageNumber, err := strconv.ParseInt(query.Get("start"), 10, 64)
+	if err != nil {
+		msg := fmt.Sprintf(`trying to parse "start" err: %q. Expecting an integer`, err)
+		return makeResp(msg, http.StatusBadRequest), nil
+	}
+
+	diskPath := resultsPagePath(terms, pageNumber)
+	return makeRespFromFile(diskPath), nil
+}
+
+func resultsPagePath(searchTerm string, pageNumber int64) string {
+	return fmt.Sprintf("./testdata/%s-%d.atom", searchTerm, pageNumber)
+}
+
+func makeRespFromFile(path string) *http.Response {
+	f, err := os.Open(path)
+	if err != nil {
+		return makeResp(err.Error(), http.StatusInternalServerError)
+	}
+
+	okResp := makeResp("200 OK", http.StatusOK)
+	okResp.Body = f
+	return okResp
+}
+
+func resultsPageFromFile(searchTerm string, pageNumber int64) *arxiv.ResultsPage {
+	path := resultsPagePath(searchTerm, pageNumber)
+	blob, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil
+	}
+	feed := new(atom.Feed)
+	if err := xml.Unmarshal(blob, feed); err != nil {
+		return nil
+	}
+	return &arxiv.ResultsPage{
+		Feed:       feed,
+		PageNumber: pageNumber,
+	}
+}
